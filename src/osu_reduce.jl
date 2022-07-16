dfbb92b41253403203b5b101bbfb4c57530ebd24
Mosè Giordano <mose@gnu.org>
2022-07-16 13:23:12 +0100

    Avoid cache in reduction benchmarks

--- src/osu_reduce.jl
+++ src/osu_reduce.jl
@@ -16,13 +16,17 @@ function OSUReduce(T::Type=Float32;
 end
 
 function osu_reduce(T::Type, bufsize::Int, iters::Int, comm::MPI.Comm)
-    send_buffer = zeros(T, bufsize)
-    recv_buffer = ones(T, bufsize)
+    cache_size = 2 ^ 16 # Assume cache size of 64 KiB
+    # To avoid hitting the cache, create buffers which are arrays of arrays of size
+    # `bufsize` so that they exceed the cache size
+    num_buffers = max(1, 2 * cache_size ÷ (sizeof(T) * bufsize))
+    send_buffer = [zeros(T, bufsize) for _ in 1:num_buffers]
+    recv_buffer = [ones(T, bufsize) for _ in 1:num_buffers]
     timer = 0.0
     MPI.Barrier(comm)
     for i in 1:iters
         tic = MPI.Wtime()
-        MPI.Reduce!(send_buffer, recv_buffer, +, comm)
+        MPI.Reduce!(@inbounds(send_buffer[mod1(i, num_buffers)]), @inbounds(recv_buffer[mod1(i, num_buffers)]), +, comm)
         toc = MPI.Wtime()
         timer += toc - tic
     end
